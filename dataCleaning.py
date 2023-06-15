5fb19eb17954657989d06d51754cd02e72af388c
Amsyar Malek <amsyar0402@gmail.com>
2023-06-15 17:12:12 +0800

    Optimize and refactor the codes

---
+++ dataCleaning.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def trim_header(filename):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(filename, nrows=0)
+    
+    # Trim whitespace, replace spaces/tabs with underscores, and convert to lowercase
+    new_header = [col.strip().replace(' ', '_').replace('\t', '_').lower() for col in df.columns]
+    
+    # Format the modified header as a multi-line string
+    header_lines = '\n'.join(new_header) + '\n'
+    
+    # Save the modified header to a new file
+    output_filename = f"trimmed_{filename}"
+    full_output_path = fr'{output_filename}.csv'
+    with open(full_output_path, 'w') as file:
+        file.write(header_lines)
+    
+    print(f"Trimmed header saved to {output_filename}")
+
+# Ask the user to input the filename
+filename = input("Enter the CSV filename: ")
+fullpath = fr'C:\Users\datamicron\Documents\Sample Dataset\CSV\{filename}.csv'
+
+# Call the function to trim the header
+trim_header(filename)
--- projects/words_count/app.py
+++ projects/words_count/app.py
@@ -2,6 +2,28 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+class WordCounter:
+    def __init__(self, word, count_with_spaces, count_type):
+        self.word = word
+        self.count_with_spaces = count_with_spaces
+        self.count_type = count_type
+
+    def count_words(self):
+        if self.count_with_spaces == "no":
+            if self.count_type == "column":
+                word_list = self.word.split()
+                return len(word_list)
+            elif self.count_type == "row":
+                lines = self.word.split('\n')
+                return sum(len(line.split()) > 0 for line in lines)
+        elif self.count_type == "column":
+            return len(self.word)
+        elif self.count_type == "row":
+            lines = self.word.split('\n')
+            return len(lines)
+
+        return 0
+
 # Route for the home page
 @app.route('/')
 def home():
@@ -12,18 +34,15 @@ def home():
 def count():
     # Get the form data
     count_with_spaces = request.form.get('count_with_spaces')
+    count_type = request.form.get('count_type')
     word = request.form.get('word')
 
-    # Count the word with or without spaces
-    if count_with_spaces != "yes":
-        # Remove spaces and tabs
-        word = word.replace(" ", "").replace("\t", "")
-    total_alphabets = len(word)
+    counter = WordCounter(word, count_with_spaces, count_type)
+    total_count = counter.count_words()
 
     # Render the result template with the output
-    return render_template('result.html', total_alphabets=total_alphabets)
+    return render_template('result.html', total_count=total_count)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
-
+    app.run(port=5001)
